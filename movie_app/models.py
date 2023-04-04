2ccac9460a981f1ce2fa497613eaa46450298d7c
DimedroLich <1DimedroL1@gmail.com>
2023-04-04 11:05:32 +0300

    Added template for main page. Added css styling for this page. Template gets objects from db and displays it on main page. 4.10 - DONE

--- movie_app/models.py
+++ movie_app/models.py
@@ -4,3 +4,8 @@ from django.db import models
 class Movie(models.Model):
     name = models.CharField(max_length=40)
     rating = models.IntegerField()
+    year = models.PositiveIntegerField(null=True)
+    budget = models.PositiveIntegerField(default=40_000_000)
+
+    def __str__(self):
+        return self.name
--- movie_app/views.py
+++ movie_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Movie
 # Create your views here.
+
+def index(request):
+    content = {
+        "movies" : Movie.objects.all()
+    }
+    return render(request,'movie_app/index.html',context=content)
