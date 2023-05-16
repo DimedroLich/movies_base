de95e6a5b2189d714f77c4bb94abc4a35b016850
dimedrol <1dimedrol1@gmail.com>
2023-05-16 09:40:56 +0300

    - Views about single actor/director refactored with DetailView

--- movie_app/urls.py
+++ movie_app/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.index,name='index'),
     path('movie/<slug:slug_movie>', views.about_movie, name='about'),
     path('directors/', views.Directors.as_view(), name='directors'),
-    path('directors/<int:director_id>',views.about_director,name='about_director'),
+    path('directors/<int:pk>', views.AboutDirector.as_view(), name='about_director'),
     path('actors/', views.Actors.as_view(), name='actors'),
-    path('actors/<int:actor_id>',views.about_actor,name='about_actor'),
+    path('actors/<int:pk>',views.AboutActor.as_view(),name='about_actor'),
 ]
--- movie_app/views.py
+++ movie_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.db.models import Count
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Movie, Director, Actor
 
@@ -37,15 +37,17 @@ class Directors(ListView):
 
 
 
-def about_director(request, director_id):
-    director = get_object_or_404(Director,id=director_id)
-    movies = director.movie_set.all()
-    context = {
-        'director': director,
-        'movies' : movies,
-    }
-    return render(request, 'movie_app/about_director.html', context=context)
+class AboutDirector(DetailView):
+    template_name = 'movie_app/about_director.html'
+    model = Director
+    extra_context = {"title" : 'Информация о режиссёре',}
 
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        self.director = context['director']
+        context['movies'] = self.director.movie_set.all()
+        return context
 
 class Actors(ListView):
     template_name = 'movie_app/directors_actors.html'
@@ -59,15 +61,31 @@ class Actors(ListView):
         return context
 
 
-def about_actor(request,actor_id):
-    actor = Actor.objects.get(id=actor_id)
-    if actor.gender == 'F':
-        gender_visualization = 'Женский'
-    else:
-        gender_visualization = 'Мужской'
-    context = {
-        "actor": actor,
-        "movies" : actor.movie_set.all(),
-        'gender' : gender_visualization,
-    }
-    return render(request, 'movie_app/about_actor.html', context=context)
+# def about_actor(request,actor_id):
+#     actor = Actor.objects.get(id=actor_id)
+#     if actor.gender == 'F':
+#         gender_visualization = 'Женский'
+#     else:
+#         gender_visualization = 'Мужской'
+#     context = {
+#         "actor": actor,
+#         "movies" : actor.movie_set.all(),
+#         'gender' : gender_visualization,
+#     }
+#     return render(request, 'movie_app/about_actor.html', context=context)
+
+
+class AboutActor(DetailView):
+    template_name = 'movie_app/about_actor.html'
+    model = Actor
+    extra_context = {'title' : 'Информация о актёре'}
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        self.actor = context['actor']
+        context["movies"] = self.actor.movie_set.all()
+        if self.actor.gender == 'F':
+            context['gender'] = 'Женский'
+        else:
+                context['gender'] = 'Мужской'
+        return context
